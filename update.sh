2c6b785fe20d4387e396a365df341d68db28e957
Francisco Troncoso <ftroncoso@uvigo.es>
2019-08-13 20:53:38 +0200

    Expose config. URL as parameter, download entire GitHub folder

--- update.sh
+++ update.sh
@@ -2,6 +2,36 @@
 
 die() { [ "$#" -eq 0 ] || echo "$*" >&2; exit 1; }
 
+download_github_file() {
+    url="$1"
+    filename="$(basename "${url}")"
+    res="$(wget "${url}" -qO-)" || \
+        die "Configuration file not found at '${url}'"
+    rt="$(echo "${res}" | tr -d '\r\n')" && \
+    echo "${rt}" | jq -r '.content' | base64 -d > "${filename}"
+}
+
+download_github_dir() {
+    url="$1"
+    res="$(wget "${url}" -qO-)" || \
+        die "Configuration directory not found at '${url}'"
+    rt="$(echo "${res}" | tr -d '\r\n')" && \
+    rfiles="$(echo "${rt}" | jq -r '.[] | select(.type == "file").url')" && \
+    rdirs="$(echo "${rt}" | jq -r '.[] | select(.type == "dir").url')" ||
+        die "Error processing response"
+    while read -r url; do
+        download_github_file "${url}"
+    done <<< "${rfiles}"
+    while read -r url; do
+        dirname="$(basename "${url}")"
+        pushd "${dirname}"
+        download_github_dir "${url}"
+        popd
+    done <<< "${rdirs}"
+}
+
+cfg_url_prefix="${1?"$(usage)"}"
+
 cd "$(dirname "$0")"
 
 # Code
@@ -13,7 +43,9 @@ if [ -n "$changed" ]; then
 fi
 
 # Config
-url="https://api.github.com/repos/frantp/iot-utils/contents/cfg/id/${IOTSR_DEVICE_ID}.conf"
+url="${cfg_url_prefix}/${IOTSR_DEVICE_ID}"
 res="$(wget "${url}" -qO-)" || \
-    die "Configuration file not found at '${url}'"
-echo "${res}" | tr -d '\r\n' | jq -r .content | base64 -d > "./etc/sreader.conf"
+    die "Configuration directory not found at '${url}'"
+pushd "etc"
+download_github_dir "${url}"
+popd
