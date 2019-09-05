388029c6a442089824778e9188545dcb610b5c2c
Francisco Troncoso <ftroncoso@uvigo.es>
2019-09-05 17:31:09 +0200

    Updated dependencies ; many fixes

--- update.sh
+++ update.sh
@@ -51,40 +51,43 @@ echo "Checking source code updates"
 git fetch > /dev/null
 changed="$(git log --oneline master..origin/master 2> /dev/null)"
 if [ -n "${force}" ] || [ -n "${changed}" ]; then
-    env_changed="$(git log --oneline master..origin/master sreader/requirements.txt 2> /dev/null)"
+    mkdir -p \"${LIB_DIR}\" \"${LOG_DIR}\"
+    cd "sreader"
+    env_changed="$(git log --oneline master..origin/master -- "requirements.txt" 2> /dev/null)"
+    cd ".."
 
     # Configuration
     echo "- Configuring"
     raspi-config nonint do_i2c 0
-    raspi-config nonint do_serial 0
+    raspi-config nonint do_serial 2
 
     # Dependencies
     echo "- Installing dependencies"
     until [ -n "${installed}" ]; do
         apt-get -qq update && \
-        apt-get -qq install curl git jq mosquitto python3 python3-venv python3-smbus python3-pil && \
+        apt-get -qq install curl git jq mosquitto python3 \
+            python3-venv python3-smbus python3-pil libopenjp2-7 && \
         installed=true || true
         if [ -z "${installed}" ]; then
             echo "Retrying installation"
             sleep 1
         fi
     done
-    systemctl enable mosquitto
+    systemctl enable mosquitto > /dev/null
 
     # Source code
     echo "- Updating source code"
     git reset --hard origin/master > /dev/null
     git submodule update --remote > /dev/null
 
-    mkdir -p \"${LIB_DIR}\" \"${LOG_DIR}\"
-
     if [ -n "${force}" ] || [ -n "${env_changed}" ]; then
         echo "- Installing Python environment"
-
         python3 -m venv "${LIB_DIR}/env"
         . "${LIB_DIR}/env/bin/activate"
         pip3 install --no-cache-dir -r "sreader/requirements.txt"
         deactivate
+    else
+        echo "- Python environment up to date"
     fi
 
     # Environment
@@ -92,31 +95,32 @@ if [ -n "${force}" ] || [ -n "${changed}" ]; then
     # - Logrotate
     cp "logrotate.conf" "${LOGROTATE_CFG}"
     # - Exe
-    echo "#/bin/sh
-\"${LIB_DIR}/env/bin/python3\" \"${SRC_DIR}/sreader/src/loop.py\" \"\$@\"" > "${MAIN_EXE}"
+    echo "#!/bin/sh
+\"${LIB_DIR}/env/bin/python3\" -u \"${SRC_DIR}/sreader/src/loop.py\" \"\$@\"" > "${MAIN_EXE}"
     chmod +x "${MAIN_EXE}"
     # - Service
     cp "sreader.service" "${SERVICES_DIR}"
-    systemctl daemon-reload
-    systemctl enable sreader
+    systemctl daemon-reload > /dev/null
+    systemctl enable sreader > /dev/null
     # - Cron job
-    echo "#/bin/sh
+    echo "#!/bin/sh
+echo \"\$(date -Ins)\" >> \"${UPDATER_LOG}\"
 \"${UPDATER_EXE}\" \"\${SREADER_CFG_URL}\" >> \"${UPDATER_LOG}\" 2>&1" > "${CRON_EXE}"
     chmod +x "${CRON_EXE}"
     # - Updater
     cp "${SRC_DIR}/update.sh" "${UPDATER_EXE}"
     chmod +x "${UPDATER_EXE}"
 else
-    echo "No updates"
+    echo "Up to date"
 fi
 
 # Config
 echo "Updating configuration files"
-download_github_file "${cfg_url}/mosquitto.conf" "/etc/mosquitto/mosquitto.conf"
+download_github_file "${cfg_url}/mosquitto.conf" "/etc/mosquitto/conf.d/sreader.conf"
 download_github_file "${cfg_url}/sreader.conf" "/etc/sreader.conf"
 
 # Restarting services
 echo "Restarting services"
-systemctl restart mosquitto sreader
+systemctl restart mosquitto sreader > /dev/null
 
 echo "Finished"
