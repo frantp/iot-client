3c52523d4ab3d4fd9684b843f409e2f9065d9e31
Francisco Troncoso <ftroncoso@uvigo.es>
2019-09-11 19:35:42 +0200

    Fixed installation

--- install.sh
+++ install.sh
@@ -13,6 +13,8 @@ SRC_DIR="/opt/iot-client"
 cfg_url="${1:-${SREADER_CFG_URL}}"
 
 echo "[$(date -Ins)] Installing IOT client"
+apt-get -qq update
+apt-get -qq install git curl
 rm -rf "${SRC_DIR}"
 git clone --recurse-submodules "https://github.com/frantp/iot-client.git" "${SRC_DIR}"
 "${SRC_DIR}/setup.sh" "${cfg_url}"
